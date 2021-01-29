b8872cdbba570bc4432fa1ff8dda44c0ff06fb7e
Alec Blake <ab587817@ohio.edu>
2021-01-29 13:12:26 -0500

    update

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Alec Blake
+ *        Date: 1/29/21
+ * Description: git lab
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+cout << "Hello Git!!" << endl;
+    /*add code*/
+    return 0;
+}// main
